165f521bb18041b6dd40fd504a8f66a2c0b6ce53
dofukuhara <dofukuhara@gmail.com>
2013-12-15 15:21:13 -0200

    Updating MainActivity

--- src/com/example/douglasfukuharastudy/MainActivity.java
+++ src/com/example/douglasfukuharastudy/MainActivity.java
@@ -3,8 +3,9 @@ package com.example.douglasfukuharastudy;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.Request;
 import com.facebook.Response;
@@ -23,6 +24,7 @@ public class MainActivity extends Activity {
 		Session.openActiveSession(this, true, new Session.StatusCallback() {
 		
 			// callback when session changes state
+			@SuppressWarnings("deprecation")
 			@Override
 			public void call(Session session, SessionState state, Exception exception) {
 				if (session.isOpened()) {
@@ -40,26 +42,10 @@ public class MainActivity extends Activity {
 					});
 				}
 		    }
+
 		});
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
 	
-	/*
-	 * (non-Javadoc)
-	 * @see android.app.Activity#onActivityResult(int, int, android.content.Intent)
-	 * 
-	 * An important part of the Facebook SDK is the Session class which manages much 
-	 * of the process of authenticating and authorizing users. Since the login flow 
-	 * for your app will require the users to transition out of, and back into, this 
-	 * Activity, we need a small amount of wiring to update the active session:
-	 * 
-	 */
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 	  super.onActivityResult(requestCode, resultCode, data);
